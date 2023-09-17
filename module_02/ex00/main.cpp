3899a2e90ead4b6af67a80b8cb68d700517db9f0
albagg3 <103680537+albagg3@users.noreply.github.com>
2023-09-17 19:01:43 +0000

    empezando fixed

--- module_02/ex00/main.cpp
+++ module_02/ex00/main.cpp
@@ -6,7 +6,20 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 17:47:01 by codespace         #+#    #+#             */
-/*   Updated: 2023/09/17 18:55:26 by codespace        ###   ########.fr       */
+/*   Updated: 2023/09/17 19:01:25 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed b( a );
+Fixed c;
+c = b;
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+return 0;
+}
